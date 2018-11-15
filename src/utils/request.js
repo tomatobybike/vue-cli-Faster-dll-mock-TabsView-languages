1e11d9c43f4be31c253969daf1f3618a38396cb4
杨琼 <yangqiong@dakaquan.com>
2018-11-15 14:07:31 +0800

    axios form-data request

--- src/utils/request.js
+++ src/utils/request.js
@@ -10,23 +10,41 @@ const service = axios.create({
 })
 
 // request拦截器
-service.interceptors.request.use(config => {
-  if (store.getters.token) {
-    config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+service.interceptors.request.use(
+  config => {
+    if (store.getters.token) {
+      config.headers['X-Token'] = getToken() // 让每个请求携带自定义token 请根据实际情况自行修改
+    }
+    // 设置form-data请求格式
+    if (config.isFormData) {
+      config.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+      config.headers.get['Content-Type'] = 'application/x-www-form-urlencoded'
+      config.transformRequest = [
+        function (data) {
+          let ret = ''
+          for (let it in data) {
+            ret +=
+              encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+          }
+          return ret
+        }
+      ]
+    }
+    return config
+  },
+  error => {
+    // Do something with request error
+    console.log(error) // for debug
+    Promise.reject(error)
   }
-  return config
-}, error => {
-  // Do something with request error
-  console.log(error) // for debug
-  Promise.reject(error)
-})
+)
 
 // respone拦截器
 service.interceptors.response.use(
   response => {
-  /**
-  * code为非20000是抛错 可结合自己业务进行修改
-  */
+    /**
+     * code为非20000是抛错 可结合自己业务进行修改
+     */
     const res = response.data
     if (res.code !== 0) {
       Message({
@@ -42,7 +60,7 @@ service.interceptors.response.use(
           type: 'warning',
           callback () {
             store.dispatch('FedLogOut').then(() => {
-              location.reload()// 为了重新实例化vue-router对象 避免bug
+              location.reload() // 为了重新实例化vue-router对象 避免bug
             })
           }
         })
@@ -53,7 +71,7 @@ service.interceptors.response.use(
     }
   },
   error => {
-    console.log('err' + error)// for debug
+    console.log('err' + error) // for debug
     Message({
       message: error.message,
       type: 'error',
--- src/views/kickerteam/index.vue
+++ src/views/kickerteam/index.vue
@@ -2,24 +2,26 @@
   <div class="app-container">
     <el-card>
       <div class="filter-container">
-        <el-button class="filter-item" type="primary" icon="el-icon-edit" size="small" @click="handleCreate">新建</el-button>
+        <el-button class="filter-item"
+                   type="primary"
+                   icon="el-icon-edit"
+                   size="small"
+                   @click="handleCreate">新建</el-button>
       </div>
-      <el-table
-              :data="list"
-              v-loading.body="listLoading" element-loading-text="Loading"
-              stripe
-              border
-              style="width: 100%">
-        <el-table-column
-                label="名称"
-                width="180">
+      <el-table :data="list"
+                v-loading.body="listLoading"
+                element-loading-text="Loading"
+                stripe
+                border
+                style="width: 100%">
+        <el-table-column label="名称"
+                         width="180">
           <template slot-scope="scope">
-            <span >{{ scope.row.name }}</span>
+            <span>{{ scope.row.name }}</span>
           </template>
         </el-table-column>
-        <el-table-column
-                label="创建时间"
-                width="180">
+        <el-table-column label="创建时间"
+                         width="180">
           <template slot-scope="scope">
             <i class="el-icon-time"></i>
             <span style="margin-left: 10px">{{ scope.row.date | moment}}</span>
@@ -27,265 +29,269 @@
         </el-table-column>
         <el-table-column label="操作">
           <template slot-scope="scope">
-            <el-button
-                    size="mini"
-                    @click="handleUpdate(scope.$index, scope.row)">编辑</el-button>
-            <el-button
-                    size="mini"
-                    type="danger"
-                    @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            <el-button size="mini"
+                       @click="handleUpdate(scope.$index, scope.row)">编辑</el-button>
+            <el-button size="mini"
+                       type="danger"
+                       @click="handleDelete(scope.$index, scope.row)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
-      <div class="pager-box" ng-show="paginator.totalNum">
+      <div class="pager-box"
+           ng-show="paginator.totalNum">
         <el-pagination :layout="elPager.layout"
-                      background
-                      :current-page="paginator.pageNum"
-                      :page-sizes="elPager.pageSizes"
-                      :page-size="paginator.displayNum"
-                      @current-change="changePage"
-                      @size-change="changePageSize"
-                      :total="paginator.totalNum">
+                       background
+                       :current-page="paginator.pageNum"
+                       :page-sizes="elPager.pageSizes"
+                       :page-size="paginator.displayNum"
+                       @current-change="changePage"
+                       @size-change="changePageSize"
+                       :total="paginator.totalNum">
         </el-pagination>
       </div>
     </el-card>
 
-
     <el-dialog :visible.sync="dialogVisible">
-      <img width="100%" :src="dialogImageUrl" alt="">
+      <img width="100%"
+           :src="dialogImageUrl"
+           alt="">
     </el-dialog>
 
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :model="formData" label-position="left"    class="demo-form-inline" label-width="70px" >
-        <el-form-item
-                prop="name"
-                label="名称"
-                :rules="rules.title"
-        >
+    <el-dialog :title="textMap[dialogStatus]"
+               :visible.sync="dialogFormVisible">
+      <el-form ref="dataForm"
+               :model="formData"
+               label-position="left"
+               class="demo-form-inline"
+               label-width="70px">
+        <el-form-item prop="name"
+                      label="名称"
+                      :rules="rules.title">
           <el-input v-model="formData.name"></el-input>
         </el-form-item>
-        <el-form-item
-                label="球队图标"
-        >
-          <el-upload
-                  action="/api/upload"
-                  list-type="picture-card"
-                  :file-list="formData.fileList"
-                  :limit="3"
-                  :on-success="handleAvatarSuccess"
-                  :on-exceed="handleExceed"
-                  :on-preview="handlePictureCardPreview"
-                  :on-remove="handleRemove">
+        <el-form-item label="球队图标">
+          <el-upload action="/api/upload"
+                     list-type="picture-card"
+                     :file-list="formData.fileList"
+                     :limit="3"
+                     :on-success="handleAvatarSuccess"
+                     :on-exceed="handleExceed"
+                     :on-preview="handlePictureCardPreview"
+                     :on-remove="handleRemove">
             <i class="el-icon-plus"></i>
           </el-upload>
         </el-form-item>
 
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button v-if="dialogStatus=='create'" type="primary" @click="createData">确定</el-button>
-        <el-button v-else type="primary" @click="updateData">确定</el-button>
+      <div slot="footer"
+           class="dialog-footer">
+        <el-button v-if="dialogStatus=='create'"
+                   type="primary"
+                   @click="createData">确定</el-button>
+        <el-button v-else
+                   type="primary"
+                   @click="updateData">确定</el-button>
         <el-button @click="dialogFormVisible = false">取消</el-button>
       </div>
     </el-dialog>
-
 
   </div>
 </template>
 
 <script>
-  import core from '@/utils/util'
-  import { getList, createTeam, updateTeam, deleteTeam } from '@/api/kickerteam'
-  export default {
-    data () {
-      return {
-        listLoading: false,
-        list: [],
-        paginator: {
-          pageNum: 1,
-          displayNum: 20,
-          totalNum: 0
-        },
-        elPager: {
-          layout: core.elPager.layout,
-          pageSizes: core.elPager.pageSizes
-        },
-        formData: {
-          id: null,
-          name: '',
-          fileList: []
-        },
-        dialogFormVisible: false,
-        dialogStatus: '',
-        textMap: {
-          update: '修改',
-          create: '新建'
-        },
-        rules: {
-          title: [{ required: true, message: '必填项', trigger: 'blur' }],
-          name: [{ required: true, message: '必填项', trigger: 'blur' }]
-        },
-        dialogImageUrl: '',
-        dialogVisible: false
+import core from '@/utils/util'
+import { getList, createTeam, updateTeam, deleteTeam } from '@/api/kickerteam'
+export default {
+  data () {
+    return {
+      listLoading: false,
+      list: [],
+      paginator: {
+        pageNum: 1,
+        displayNum: 20,
+        totalNum: 0
+      },
+      elPager: {
+        layout: core.elPager.layout,
+        pageSizes: core.elPager.pageSizes
+      },
+      formData: {
+        id: null,
+        name: '',
+        fileList: []
+      },
+      dialogFormVisible: false,
+      dialogStatus: '',
+      textMap: {
+        update: '修改',
+        create: '新建'
+      },
+      rules: {
+        title: [{ required: true, message: '必填项', trigger: 'blur' }],
+        name: [{ required: true, message: '必填项', trigger: 'blur' }]
+      },
+      dialogImageUrl: '',
+      dialogVisible: false
+    }
+  },
+  created () {
+    console.log(core.isMobile)
+    console.log(core.elPager.layout)
+    this.getList()
+  },
+  methods: {
+    getList () {
+      let params = {
+        pageNum: this.paginator.pageNum,
+        displayNum: this.paginator.displayNum
       }
+      const loading = this.$loading(this.Global.loadingOption)
+      getList(params).then(response => {
+        loading.close()
+        this.list = response.data.list
+        Object.assign(this.paginator, {
+          totalNum: response.data.total
+        })
+      }).catch(error => {
+        loading.close()
+        console.log(error)
+      })
     },
-    created () {
-      console.log(core.isMobile)
-      console.log(core.elPager.layout)
+    changePage (id) {
+      this.paginator.pageNum = id
       this.getList()
     },
-    methods: {
-      getList () {
-        let params = {
-          pageNum: this.paginator.pageNum,
-          displayNum: this.paginator.displayNum
+    changePageSize (pageSize) {
+      this.paginator.displayNum = pageSize
+      this.changePage(1)
+    },
+    handleUpdate (index, row) {
+      this.formData = Object.assign({}, row) // copy obj
+      console.log('row', row)
+      this.dialogStatus = 'update'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    handleDelete (index, row) {
+      this.$confirm('确定要删除规则？').then(() => {
+        const param = {
+          id: row.id
         }
-        const loading = this.$loading(this.Global.loadingOption)
-        getList(params).then(response => {
+        let loading = this.$loading(this.Global.loadingOption)
+        deleteTeam(param).then(() => {
           loading.close()
-          this.list = response.data.list
-          Object.assign(this.paginator, {
-            totalNum: response.data.total
+          this.getList()
+          this.dialogFormVisible = false
+          this.$notify({
+            title: '成功',
+            message: '删除成功',
+            type: 'success',
+            duration: 2000
           })
-        }).catch(error => {
+        }).catch(() => {
           loading.close()
-          console.log(error)
         })
-      },
-      changePage (id) {
-        this.paginator.pageNum = id
-        this.getList()
-      },
-      changePageSize (pageSize) {
-        this.paginator.displayNum = pageSize
-        this.changePage(1)
-      },
-      handleUpdate (index, row) {
-        this.formData = Object.assign({}, row) // copy obj
-        console.log('row', row)
-        this.dialogStatus = 'update'
-        this.dialogFormVisible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].clearValidate()
-        })
-      },
-      handleDelete (index, row) {
-        this.$confirm('确定要删除规则？').then(() => {
-          const param = {
-            id: row.id
+      }).catch(error => {
+        console.log(error)
+      })
+    },
+    handleCreate () {
+      this.resetTemp()
+      this.dialogStatus = 'create'
+      this.dialogFormVisible = true
+      this.$nextTick(() => {
+        this.$refs['dataForm'].clearValidate()
+      })
+    },
+    resetTemp () {
+      this.formData = {
+        id: null,
+        name: '',
+        fileList: []
+      }
+    },
+    createData () {
+      this.$refs['dataForm'].validate((valid) => {
+        console.log('valid', valid)
+        if (valid) {
+          this.getUrlList()
+          let param = {
+            name: this.formData.name,
+            fileList: this.formData.urls
           }
           let loading = this.$loading(this.Global.loadingOption)
-          deleteTeam(param).then(() => {
+          createTeam(param).then(() => {
             loading.close()
             this.getList()
             this.dialogFormVisible = false
             this.$notify({
               title: '成功',
-              message: '删除成功',
+              message: '创建成功',
               type: 'success',
               duration: 2000
             })
-          }).catch(() => {
+          }).catch(error => {
             loading.close()
-          })
-        }).catch(error => {
-          console.log(error)
-        })
-      },
-      handleCreate () {
-        this.resetTemp()
-        this.dialogStatus = 'create'
-        this.dialogFormVisible = true
-        this.$nextTick(() => {
-          this.$refs['dataForm'].clearValidate()
-        })
-      },
-      resetTemp () {
-        this.formData = {
-          id: null,
-          name: '',
-          fileList: []
-        }
-      },
-      createData () {
-        this.$refs['dataForm'].validate((valid) => {
-          console.log('valid', valid)
-          if (valid) {
-            this.getUrlList()
-            let param = {
-              name: this.formData.name,
-              fileList: this.formData.urls
-            }
-            let loading = this.$loading(this.Global.loadingOption)
-            createTeam(param).then(() => {
-              loading.close()
-              this.getList()
-              this.dialogFormVisible = false
-              this.$notify({
-                title: '成功',
-                message: '创建成功',
-                type: 'success',
-                duration: 2000
-              })
-            }).catch(error => {
-              loading.close()
-              console.log(error)
-            })
-          }
-        })
-      },
-      updateData () {
-        this.$refs['dataForm'].validate((valid) => {
-          if (valid) {
-            this.getUrlList()
-            let param = {
-              id: this.formData.id,
-              name: this.formData.name,
-              fileList: this.formData.urls
-            }
-            let loading = this.$loading(this.Global.loadingOption)
-            updateTeam(param).then(() => {
-              loading.close()
-              this.getList()
-              this.dialogFormVisible = false
-              this.$notify({
-                title: '成功',
-                message: '更新成功',
-                type: 'success',
-                duration: 2000
-              })
-            }).catch(error => {
-              loading.close()
-              console.log(error)
-            })
-          }
-        })
-      },
-      getUrlList () {
-        let newArray = []
-        for (var value of this.fileList) {
-          newArray.push({
-            url: value.url
+            console.log(error)
           })
         }
-        this.formData.urls = newArray
-      },
-      handleAvatarSuccess (res, file, fileList) {
-        fileList.find(function (value, index, arr) {
-          if (value.uid === file.uid) {
-            value.url = res.data
+      })
+    },
+    updateData () {
+      this.$refs['dataForm'].validate((valid) => {
+        if (valid) {
+          this.getUrlList()
+          let param = {
+            id: this.formData.id,
+            name: this.formData.name,
+            fileList: this.formData.urls
           }
+          let loading = this.$loading(this.Global.loadingOption)
+          updateTeam(param).then(() => {
+            loading.close()
+            this.getList()
+            this.dialogFormVisible = false
+            this.$notify({
+              title: '成功',
+              message: '更新成功',
+              type: 'success',
+              duration: 2000
+            })
+          }).catch(error => {
+            loading.close()
+            console.log(error)
+          })
+        }
+      })
+    },
+    getUrlList () {
+      let newArray = []
+      for (var value of this.fileList) {
+        newArray.push({
+          url: value.url
         })
-        this.fileList = fileList
-      },
-      handleRemove (file, fileList) {
-      },
-      handlePictureCardPreview (file) {
-        this.dialogImageUrl = file.url
-        this.dialogVisible = true
-      },
-      handleExceed (files, fileList) {
-        this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
       }
+      this.formData.urls = newArray
+    },
+    handleAvatarSuccess (res, file, fileList) {
+      fileList.find(function (value, index, arr) {
+        if (value.uid === file.uid) {
+          value.url = res.data
+        }
+      })
+      this.fileList = fileList
+    },
+    handleRemove (file, fileList) {
+    },
+    handlePictureCardPreview (file) {
+      this.dialogImageUrl = file.url
+      this.dialogVisible = true
+    },
+    handleExceed (files, fileList) {
+      this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`)
     }
   }
+}
 </script>
